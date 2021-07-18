100492ebdfe98e1e0b50761e6fe26d0b3b61b521
LinuzJ <linusjern00@gmail.com>
2021-07-18 21:41:16 +0300

    users and post models for graphql

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+// dependencies
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
+
+// "local" imports
+const Post = require("./models/Post");
 const dotenv = require("dotenv");
 require("dotenv").config();
 
+// main
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    username: String!
+    createdAt: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Test1",
+    async getPosts() {
+      try {
+        const recieved = await Post.find();
+        return recieved;
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
   },
 };
 
@@ -29,8 +47,7 @@ mongoose
   .then(() => {
     console.log("MONGODB conn successful");
     return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server is up and running at ${res.url}`);
   });
-
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server is up and running at ${res.url}`);
-});
