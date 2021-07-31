c8e2a4424f2cb60a37fee6214dd73533bf6753d0
LinuzJ <linusjern00@gmail.com>
2021-07-31 15:13:12 +0300

    delete Post added

--- graphql/resolvers/checkAuth.js
+++ graphql/resolvers/checkAuth.js
@@ -1,30 +1,30 @@
-const { AuthenticationError } = require('apollo-server')
+const { AuthenticationError } = require("apollo-server");
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 require("dotenv").config();
 
 module.exports = (context) => {
-    // Here the context will include many headers of which one is the auth
-    const authHeader = context.req.headers.authorization;
+  // Here the context will include many headers of which one is the auth
+  const authHeader = context.req.headers.authorization;
 
-    if (authHeader) {
-        // Build structure in auth Header -> Bearer ... 
-        // So we split the authheader and take the second element
-        const token = authHeader.split('Bearer ')[1];
+  if (authHeader) {
+    // Build structure in auth Header -> Bearer ...
+    // So we split the authheader and take the second element
+    const token = authHeader.split("Bearer ")[1];
 
-        // check if this really exists
-        if (token) {
-           try {
-               const user = jwt.verify(token, process.env.SECRET_KEY);
-               return user;
-           } catch (e) {
-                throw new AuthenticationError('Invalid token');
-           }
-        }
-        // If TOKEN fails
-        throw new Error('Authentication token in wrong format');
+    // check if this really exists
+    if (token) {
+      try {
+        const user = jwt.verify(token, process.env.SECRET_KEY);
+        return user;
+      } catch (e) {
+        throw new AuthenticationError("Invalid token");
+      }
     }
-    // If we get no authHeader
-    throw new Error('Authorization header not given');
+    // If TOKEN fails
+    throw new Error("Authentication token in wrong format");
+  }
+  // If we get no authHeader
+  throw new Error("Authorization header not given");
 };
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,17 +1,18 @@
 const Post = require("../../models/Post");
 const checkAuth = require("./checkAuth");
+const { AuthenticationError } = require("apollo-server");
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const recieved = await Post.find();
+        const recieved = await Post.find().sort({ createdAt: -1 });
         return recieved;
       } catch (e) {
         throw new Error(e);
       }
     },
-    async getPost(_, { postId }){
+    async getPost(_, { postId }) {
       try {
         const post = await Post.findById(postId);
         if (post) {
@@ -19,27 +20,46 @@ module.exports = {
         } else {
           throw new Error("POST NOT FOUND");
         }
-      } catch(e) {
+      } catch (e) {
         throw new Error(e);
-    }
-    }
+      }
+    },
   },
+  // THEN MUTATIONS
   Mutation: {
+    // ---------------------- CREATE POST --------------------------
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      console.log(user);
       // If the checkAuth function does not throw an error and we actually get a user back
       const newPost = new Post({
         body,
         user: user.id,
         username: user.username,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
       });
 
       // Then actually save the new post
       const post = await newPost.save();
 
       return post;
-    }
-  }
+    },
+    // ---------------------- DELETE POST --------------------------
+    async deletePost(_, { postId }, context) {
+      // Find and check the user
+      const user = checkAuth(context);
+
+      // Then make sure the user is the author of the post, then delete the post
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted!";
+        } else {
+          throw new AuthenticationError("This is not your post!");
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
+  },
 };
