dede2adb3710da9bbd06f9d50f7ddcd41e3fdf3a
LinuzJ <linusjern00@gmail.com>
2021-08-05 22:44:50 +0300

    createPost bugfix

--- graphql/resolvers/like_comment.js
+++ graphql/resolvers/like_comment.js
@@ -4,33 +4,34 @@ const { UserInputError } = require("apollo-server");
 const User = require("../../models/User");
 
 module.exports = {
-    Mutation: {
-        createComment: async (_, { postId, body }, context) => {
-            // First check if user is logged in via the checkAuth function
-            const user = checkAuth(context);
+  Mutation: {
+    createComment: async (_, { postId, body }, context) => {
+      // First check if user is logged in via the checkAuth function
+      const user = checkAuth(context);
 
-            // Then filter out cases where the comment is empty
-            if ( body.trim() === '' ) {
-                throw new UserInputError('Empty comment', {
-                    errors: {
-                        body: 'Comment body cannot be empty'
-                    }
-                })
-            }
+      // Then filter out cases where the comment is empty
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
+          errors: {
+            body: "Comment body cannot be empty",
+          },
+        });
+      }
 
-            // Then get the post object from ou db 
-            const post = await Post.findById(postId);
+      // Then get the post object from ou db
+      const post = await Post.findById(postId);
 
-            // Add the comment if the post exists, then save it to the db. Else throw error
-            if ( post ) {
-                post.comments.unshift({
-                    body,
-                    username,
-                    createdAt: new Date().toISOString()
-                })
-                await post.save();
-                return post;
-            } else throw new UserInputError('Post is not availible')
-        }
-    }
-}
+      // Add the comment if the post exists, then save it to the db. Else throw error
+      if (post) {
+        post.comments.unshift({
+          body,
+          user: user.username,
+          createdAt: new Date().toISOString(),
+        });
+        // Save the post, then return the object
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post is not availible");
+    },
+  },
+};
