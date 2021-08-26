767a6b513015d500bcd922bc4604f7ad57b2f3be
LinuzJ <linusjern00@gmail.com>
2021-08-26 23:08:09 +0300

    Import setup correct

--- front-end/src/ApolloProvider.js
+++ front-end/src/ApolloProvider.js
@@ -1,10 +1,10 @@
 import React from "react";
 import App from "./App";
-import ApolloClient from "apollo-client";
+import { ApolloClient } from "@apollo/client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
-const dotenv = require("../dotenv");
+const dotenv = require("dotenv");
 require("dotenv").config();
 
 const linkToBackend = createHttpLink({
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Hej</p>
+      <p>Hej you</p>
     </div>
   );
 }
