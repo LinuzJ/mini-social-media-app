33efcc0f349ce4b5cde81e6cc6be84cdfae6de65
LinuzJ <linusjern00@gmail.com>
2021-08-29 16:53:27 +0300

    uri to backend works, apollo works

--- front-end/src/ApolloProvider.js
+++ front-end/src/ApolloProvider.js
@@ -1,14 +1,14 @@
 import React from "react";
 import App from "./App";
-import { ApolloClient } from "@apollo/client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
-require("dotenv").config();
+import { ApolloClient } from "apollo-client";
 
 const linkToBackend = createHttpLink({
-  uri: process.env.URI_TO_BACKEND,
+  uri: process.env.REACT_APP_URI_TO_BACKEND,
 });
+console.log(process.env);
 
 const client_apollo = new ApolloClient({
   link: linkToBackend,
