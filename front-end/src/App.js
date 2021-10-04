81091a2bac5571b5afa69d8cc78903bc26d52c31
LinuzJ <linusjern00@gmail.com>
2021-10-05 00:13:49 +0300

    login page and route added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import Register from "./Register";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Home} />
       <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
     </Router>
   );
 }
