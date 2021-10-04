33b43073825574b995dc249f7c36ace46ad4c8ee
LinuzJ <linusjern00@gmail.com>
2021-10-05 00:12:08 +0300

    register page and route adde

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
+import Register from "./Register";
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
     </Router>
   );
 }
