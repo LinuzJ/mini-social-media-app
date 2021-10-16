f4193e415eba20c7d5fce8a7bacf71689ec70678
LinuzJ <linusjern00@gmail.com>
2021-10-16 15:43:07 +0300

    query works

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
+import PostBox from "../components/ostBox";
 
+const POSTS_QUERY = gql`
+  query {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likesAmount
+      likes {
+        username
+      }
+      commentsAmount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 function Home() {
-  return <p>Home page</p>;
+  const {
+    loading,
+    data: { getPosts: posts },
+  } = useQuery(POSTS_QUERY);
+  if (data) {
+    console.log(data);
+  }
+  return (
+    <Grid columns={3} divided>
+      <Grid.Row>
+        <h1>POSTS</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <p>Loading Posts</p>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostBox post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
+  );
 }
 
 export default Home;
