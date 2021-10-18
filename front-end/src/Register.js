80ac6a70dac8eb2429bb6f73442c3425a78d81f6
LinuzJ <linusjern00@gmail.com>
2021-10-18 13:11:15 +0300

    halfway through registration (not done)

--- front-end/src/Register.js
+++ front-end/src/Register.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { Icon, Button, Form } from "semantic-ui-react";
+import gql from "graphql-tag";
+
+const REGISTER_USER = gql`
+  mutation register(
+    $username: String!
+    $email: String!
+    $username: String!
+    $username: String!
+  )
+`;
 
 function Register() {
-  return <p>Register page</p>;
+  // State for input variables, used for login verification
+  const [inputs, setInputs] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+  // onChange to change the values of the variables
+  const onChange = (event) => {
+    setInputs({ ...inputs, [event.target.name]: event.target.value });
+  };
+  // onSubmit for registration
+  const onSubmit = (event) => {};
+  return (
+    <>
+      <Form onSubmit={onSubmit} noValidate>
+        <h1>
+          <Form.Input
+            label="Username"
+            placeholder="exampleUsername123"
+            name="username"
+            value={inputs.username}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Email"
+            placeholder="example.email@emai.com"
+            name="email"
+            value={inputs.email}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Password"
+            placeholder="*********"
+            name="password"
+            value={inputs.password}
+            onChange={onChange}
+          />
+          <Form.Input
+            label="Confirm Password"
+            placeholder="*********"
+            name="confirmPassword"
+            value={inputs.confirmPassword}
+            onChange={onChange}
+          />
+          <Button icon labelPosition="right">
+            Submit
+            <Icon name="right arrow" />
+          </Button>
+        </h1>
+      </Form>
+    </>
+  );
 }
 
 export default Register;
