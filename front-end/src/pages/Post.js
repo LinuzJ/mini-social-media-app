aed5b0fa78a8e35c3810845f2fc11c55999909ca
LinuzJ <linusjern00@gmail.com>
2021-11-07 15:18:46 +0200

    content on page

--- front-end/src/pages/Post.js
+++ front-end/src/pages/Post.js
@@ -1,5 +1,65 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import queries from "../utils/queries";
 
-const Post = (props) => {};
+// Style
+import {
+  Segment,
+  Dimmer,
+  Loader,
+  Grid,
+  GridColumn,
+  Card,
+  CardContent,
+  Image,
+} from "semantic-ui-react";
+import moment from "moment";
+
+const Post = (props) => {
+  // Get post id from url
+  const postId = props.match.params.PostId;
+
+  // query
+  const { loading, data } = useQuery(queries.GET_POST_QUERY, {
+    variables: {
+      postId,
+    },
+  });
+
+  let layout; // init layour variable
+
+  if (loading) {
+    layout = (
+      <Dimmer active inverted>
+        <Loader inverted>Loading</Loader>
+      </Dimmer>
+    );
+  } else {
+    const post = data.getPost;
+    layout = (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image
+              floated="right"
+              size="mini"
+              src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+            />
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{post.username}</Card.Header>
+                <Card.Meta>{moment(post.createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{post.body}</Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+  return layout;
+};
 
 export default Post;
